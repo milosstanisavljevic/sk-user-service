483892c057b6d9b6c2184d20eb1ba3a1b1034eab
milosstanisavljevic <mstanisavljevic8220r@raf.rs>
2022-01-14 14:30:34 +0100

    pom.xml update

--- src/test/java/com/raf/sk/SkProjekatApplicationTests.java
+++ src/test/java/com/raf/sk/SkProjekatApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SkProjekatApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
